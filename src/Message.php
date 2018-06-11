df30e55f3ff7e6e07aff885fcccb2539fa358125
Brian Faust <hello@brianfaust.me>
2018-06-11 07:51:18 +0300

    feat: message builder

---
+++ src/Message.php
@@ -0,0 +1,150 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ark PHP Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ArkEcosystem\Crypto;
+
+use BitWasp\Bitcoin\Crypto\Hash;
+use BitWasp\Bitcoin\Key\PublicKeyFactory;
+use BitWasp\Bitcoin\Signature\SignatureFactory;
+use BitWasp\Buffertools\Buffer;
+
+/**
+ * This is the message class.
+ *
+ * @author Brian Faust <brian@ark.io>
+ */
+class Message
+{
+    /**
+     * The message signer public key.
+     *
+     * @var string
+     */
+    public $publicKey;
+
+    /**
+     * The message signature.
+     *
+     * @var string
+     */
+    public $signature;
+
+    /**
+     * The message contents.
+     *
+     * @var string
+     */
+    public $message;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param array $message
+     */
+    public function __construct(array $message)
+    {
+        $this->publicKey = $message['publicKey'];
+        $this->signature = $message['signature'];
+        $this->message   = $message['message'];
+    }
+
+    /**
+     * Convert the message to its JSON representation.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    /**
+     * Create a new message instance from an array.
+     *
+     * @param array $message
+     *
+     * @return \ArkEcosystem\Crypto\Message
+     */
+    public static function fromArray(array $message): self
+    {
+        return new static($message);
+    }
+
+    /**
+     * Create a new message instance from a JSON string.
+     *
+     * @param string $message
+     *
+     * @return \ArkEcosystem\Crypto\Message
+     */
+    public static function fromJSON(string $message): self
+    {
+        return new static(json_decode($message, true));
+    }
+
+    /**
+     * [signMessage description].
+     *
+     * @param string $message
+     * @param string $passphrase
+     *
+     * @return \ArkEcosystem\Crypto\Message
+     */
+    public static function sign(string $message, string $passphrase): self
+    {
+        $keys = Crypto::getKeys($passphrase);
+
+        return new static([
+            'publicKey' => $keys->getPublicKey()->getHex(),
+            'signature' => $keys->sign(Hash::sha256(new Buffer($message)))->getBuffer()->getHex(),
+            'message'   => $message,
+        ]);
+    }
+
+    /**
+     * Verify the.
+     *
+     * @return bool
+     */
+    public function verify(): bool
+    {
+        return PublicKeyFactory::fromHex($this->publicKey)->verify(
+            new Buffer(hash('sha256', $this->message, true)),
+            SignatureFactory::fromHex($this->signature)
+        );
+    }
+
+    /**
+     * Convert the message to its array representation.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'publicKey' => $this->publicKey,
+            'signature' => $this->signature,
+            'message'   => $this->message,
+        ];
+    }
+
+    /**
+     * Convert the message to its JSON representation.
+     *
+     * @return string
+     */
+    public function toJson(): string
+    {
+        return json_encode($this->toArray());
+    }
+}
---
+++ tests/MessageTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ark PHP Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ArkEcosystem\Tests\Crypto;
+
+use ArkEcosystem\Crypto\Crypto;
+use ArkEcosystem\Crypto\Message;
+
+/**
+ * This is the blocks resource test class.
+ *
+ * @author Brian Faust <brian@ark.io>
+ * @coversNothing
+ */
+class MessageTest extends TestCase
+{
+    /** @test */
+    public function it_should_create_a_message_from_an_array()
+    {
+        $rawMessage = $this->getRawMessage();
+
+        $message = Message::fromArray($rawMessage);
+
+        $this->assertSame($message->publicKey, $rawMessage['publicKey']);
+        $this->assertSame($message->signature, $rawMessage['signature']);
+        $this->assertSame($message->message, $rawMessage['message']);
+    }
+
+    /** @test */
+    public function it_should_create_a_message_from_a_string()
+    {
+        $rawMessage = $this->getRawMessage();
+
+        $message = Message::fromJSON(json_encode($rawMessage));
+
+        $this->assertSame($message->publicKey, $rawMessage['publicKey']);
+        $this->assertSame($message->signature, $rawMessage['signature']);
+        $this->assertSame($message->message, $rawMessage['message']);
+    }
+
+    /** @test */
+    public function it_should_sign_a_message()
+    {
+        $message = Message::sign('Hello World', 'secret');
+
+        $this->assertInstanceOf(Message::class, $message);
+    }
+
+    /** @test */
+    public function it_should_verify_a_message()
+    {
+        $message = Message::fromArray($this->getRawMessage());
+
+        $this->assertTrue($message->verify());
+    }
+
+    /** @test */
+    public function it_should_turn_a_message_into_an_array()
+    {
+        $message = Message::fromArray($this->getRawMessage());
+
+        $this->assertInternalType('array', $message->toArray());
+    }
+
+    /** @test */
+    public function it_should_turn_a_message_into_a_string()
+    {
+        $message = Message::fromArray($this->getRawMessage());
+
+        $this->assertInternalType('string', $message->toJSON());
+    }
+
+    private function getRawMessage(): array
+    {
+        return [
+            'publicKey' => '03be686ed7f0539affbaf634f3bcc2b235e8e220e7be57e9397ab1c14c39137eb4',
+            'signature' => '304402206bc3a1ad7107caeeae96594e58d782fd2e90e421c0c50d9b748630a4c363e54e022068c9ea170cac8ea22b44af5ff27697c1c14d8bc5beedb7d5e48411f257489a38',
+            'message'   => 'Hello World',
+        ];
+    }
+}
