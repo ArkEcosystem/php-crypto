64a79db2c2629a285bfd10fbbe5690459ad47ad3
Brian Faust <hello@brianfaust.me>
2018-06-25 06:23:46 +0300

    feat: Private Key identity utility

---
+++ src/Identity/PrivateKey.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ark PHP Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ArkEcosystem\Crypto\Identity;
+
+use BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Key\PrivateKey as ECKey;
+use BitWasp\Bitcoin\Crypto\Hash;
+use BitWasp\Bitcoin\Key\PrivateKeyFactory;
+use BitWasp\Buffertools\Buffer;
+
+/**
+ * This is the private key class.
+ *
+ * @author Brian Faust <brian@ark.io>
+ */
+class PrivateKey
+{
+    /**
+     * Derive the private key from the given secret.
+     *
+     * @param string $secret
+     *
+     * @return \BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Key\PrivateKey
+     */
+    public static function fromSecret(string $secret): ECKey
+    {
+        $secret = Hash::sha256(new Buffer($secret));
+
+        return PrivateKeyFactory::fromHex($secret, true);
+    }
+}
