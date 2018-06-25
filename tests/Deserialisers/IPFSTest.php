fb5f3f5f3652d5339af5bcad53e73c3b7682d7fd
Brian Faust <hello@brianfaust.me>
2018-06-25 08:30:46 +0300

    test: prepare serialiser and deserialiser tests

---
+++ tests/Deserialisers/IPFSTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ark PHP Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ArkEcosystem\Tests\Crypto\Deserialisers;
+
+/**
+ * This is the ipfs deserialiser test class.
+ *
+ * @author Brian Faust <brian@ark.io>
+ * @coversNothing
+ */
+class IPFSTest extends TestCase
+{
+    /** @test */
+    public function it_should_serialise_the_transaction()
+    {
+    }
+}
