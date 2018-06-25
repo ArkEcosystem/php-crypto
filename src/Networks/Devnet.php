93efda5ef98b0ed24b87f49e6d695d3b6c6c67c9
Brian Faust <hello@brianfaust.me>
2018-06-25 07:12:40 +0300

    feat: network configurations

---
+++ src/Networks/Devnet.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ark PHP Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ArkEcosystem\Crypto\Networks;
+
+use BitWasp\Bitcoin\Network\Network as TokenNetwork;
+use BitWasp\Bitcoin\Network\NetworkFactory;
+
+/**
+ * This is the devnet network class.
+ *
+ * @author Brian Faust <brian@ark.io>
+ */
+class Devnet extends Network
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getMessagePrefix(): string
+    {
+        return "DARK message:\n";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getNethash(): string
+    {
+        return '578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getWIF(): int
+    {
+        return 170;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getFactory(): TokenNetwork
+    {
+        return NetworkFactory::create('1e', '00', '00')
+            ->setHDPubByte('46090600')
+            ->setHDPrivByte('46089520');
+    }
+}
---
+++ src/Networks/Network.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ark PHP Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ArkEcosystem\Crypto\Networks;
+
+use BitWasp\Bitcoin\Network\Network as TokenNetwork;
+
+/**
+ * This is the abstract network class.
+ *
+ * @author Brian Faust <brian@ark.io>
+ */
+abstract class Network
+{
+    /**
+     * Call a method on the network instance.
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @return mixed
+     */
+    public static function __callStatic(string $method, array $args)
+    {
+        return static::getFactory()->{$method}(...$args);
+    }
+
+    /**
+     * Get the message prefix of the network.
+     *
+     * @return string
+     */
+    abstract public static function getMessagePrefix(): string;
+
+    /**
+     * Get the nethash of the network.
+     *
+     * @return string
+     */
+    abstract public static function getNethash(): string;
+
+    /**
+     * Get the wif prefix of the network.
+     *
+     * @return int
+     */
+    abstract public static function getWIF(): int;
+
+    /**
+     * [getFactory description].
+     *
+     * @return \BitWasp\Bitcoin\Network\Network
+     */
+    abstract public static function getFactory(): TokenNetwork;
+}
