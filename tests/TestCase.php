8fd29cd22671a6ca444e81c8575d030dc83b2340
Brian Faust <hello@brianfaust.me>
2018-06-25 14:31:58 +0300

    test: setup fixtures

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,4 +17,10 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function getTransactionType(int $type): object
+    {
+        $path = __DIR__."/fixtures/Transactions/type-{$type}.json";
+
+        return json_decode(file_get_contents($path));
+    }
 }
