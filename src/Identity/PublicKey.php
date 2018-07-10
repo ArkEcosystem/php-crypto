6dd12662af24019c4405a1a026c5d9adc05339c6
Brian Faust <hello@brianfaust.me>
2018-07-10 18:16:23 +0300

    feat: public key from hex and validate

--- src/Identity/PublicKey.php
+++ src/Identity/PublicKey.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace ArkEcosystem\Crypto\Identity;
 
 use BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Key\PublicKey as EcPublicKey;
+use BitWasp\Bitcoin\Key\PublicKeyFactory;
 
 /**
  * This is the public key class.
@@ -32,5 +33,29 @@ class PublicKey
     public static function fromSecret(string $secret): EcPublicKey
     {
         return PrivateKey::fromSecret($secret)->getPublicKey();
+    }
+
+    /**
+     * Create a public key instance from a hex string.
+     *
+     * @param \BitWasp\Buffertools\BufferInterface|string $publicKey
+     *
+     * @return \BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Key\PublicKey
+     */
+    public static function fromHex($publicKey): EcPublicKey
+    {
+        return PublicKeyFactory::fromHex($publicKey);
+    }
+
+    /**
+     * Validate the given public key.
+     *
+     * @param \BitWasp\Buffertools\BufferInterface|string $publicKey
+     *
+     * @return bool
+     */
+    public static function validate(string $publicKey): bool
+    {
+        return PublicKeyFactory::validateHex($publicKey);
     }
 }
--- tests/Identity/PublicKeyTest.php
+++ tests/Identity/PublicKeyTest.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace ArkEcosystem\Tests\Crypto\Identity;
 
 use ArkEcosystem\Crypto\Identity\PublicKey as TestClass;
-use ArkEcosystem\Crypto\Networks\Devnet;
 use ArkEcosystem\Tests\Crypto\TestCase;
 use BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Key\PublicKey as EcPublicKey;
 
@@ -29,10 +28,26 @@ class PublicKeyTest extends TestCase
     /** @test */
     public function it_should_get_the_public_key_from_secret()
     {
-        $actual = TestClass::fromSecret('this is a top secret passphrase', Devnet::new());
+        $actual = TestClass::fromSecret('this is a top secret passphrase');
 
         $this->assertInstanceOf(EcPublicKey::class, $actual);
         $this->assertInternalType('string', $actual->getHex());
         $this->assertSame('034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192', $actual->getHex());
+    }
+
+    /** @test */
+    public function it_should_get_the_public_key_from_hex()
+    {
+        $actual = TestClass::fromHex('034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192');
+
+        $this->assertSame('034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192', $actual->getHex());
+    }
+
+    /** @test */
+    public function it_should_validate_the_public_key()
+    {
+        $actual = TestClass::validate('034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192');
+
+        $this->assertTrue($actual);
     }
 }
