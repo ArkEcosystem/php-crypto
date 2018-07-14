56e935d343df37c65d3e1f9d9237c488905a749c
Brian Faust <hello@brianfaust.me>
2018-07-14 08:41:04 +0300

    test: add vendor field tests for transfer

--- tests/Deserializers/TransferTest.php
+++ tests/Deserializers/TransferTest.php
@@ -31,7 +31,7 @@ class TransferTest extends TestCase
     {
         $transaction = $this->getTransactionFixtureWithPassphrase(0);
 
-        $actual = Deserializer::new($transaction->serialized)->deserialize();
+        $actual = Deserializer::new($transaction['serialized'])->deserialize();
 
         $this->assertTransaction($transaction, $actual);
     }
@@ -41,25 +41,45 @@ class TransferTest extends TestCase
     {
         $transaction = $this->getTransactionFixtureWithSecondPassphrase(0);
 
-        $actual = Deserializer::new($transaction->serialized)->deserialize();
+        $actual = Deserializer::new($transaction['serialized'])->deserialize();
 
         $this->assertTransaction($transaction, $actual);
-        $this->assertSame($transaction->data->signSignature, $actual->signSignature);
+        $this->assertSame($transaction['data']['signSignature'], $actual->signSignature);
+    }
+
+    /** @test */
+    public function it_should_deserialize_the_transaction_signed_with_a_passphrase_and_vendor_field()
+    {
+        $transaction = $this->getTransactionFixture(0, 'passphrase-with-vendor-field');
+
+        $actual = Deserializer::new($transaction['serialized'])->deserialize();
+
+        $this->assertTransaction($transaction, $actual);
+    }
+
+    /** @test */
+    public function it_should_deserialize_the_transaction_signed_with_a_second_passphrase_and_vendor_field()
+    {
+        $transaction = $this->getTransactionFixture(0, 'second-passphrase-with-vendor-field');
+
+        $actual = Deserializer::new($transaction['serialized'])->deserialize();
+
+        $this->assertTransaction($transaction, $actual);
     }
 
     private function assertTransaction($transaction, $actual)
     {
         $this->assertSame(1, $actual->version);
         $this->assertSame(30, $actual->network);
-        $this->assertSame($transaction->data->type, $actual->type);
-        $this->assertSame($transaction->data->timestamp, $actual->timestamp);
-        $this->assertSame($transaction->data->senderPublicKey, $actual->senderPublicKey);
-        $this->assertSame($transaction->data->fee, $actual->fee);
-        $this->assertSame($transaction->data->amount, $actual->amount);
+        $this->assertSame($transaction['data']['type'], $actual->type);
+        $this->assertSame($transaction['data']['timestamp'], $actual->timestamp);
+        $this->assertSame($transaction['data']['senderPublicKey'], $actual->senderPublicKey);
+        $this->assertSame($transaction['data']['fee'], $actual->fee);
+        $this->assertSame($transaction['data']['amount'], $actual->amount);
         $this->assertSame(0, $actual->expiration);
-        $this->assertSame($transaction->data->recipientId, $actual->recipientId);
-        $this->assertSame($transaction->data->signature, $actual->signature);
-        $this->assertSame($transaction->data->id, $actual->id);
-        $this->assertSame($transaction->serialized, Serializer::new($actual)->serialize()->getHex());
+        $this->assertSame($transaction['data']['recipientId'], $actual->recipientId);
+        $this->assertSame($transaction['data']['signature'], $actual->signature);
+        $this->assertSame($transaction['data']['id'], $actual->id);
+        $this->assertSame($transaction['serialized'], Serializer::new($actual->toArray())->serialize()->getHex());
     }
 }
