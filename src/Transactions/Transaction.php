1d8eb7dc735dc3005994ddfbd746f5c535a4fd0d
zillionn <zillionn@users.noreply.github.com>
2018-11-03 14:42:19 +0200

    fix: maximum vendor field length (#25)

--- src/Transactions/Transaction.php
+++ src/Transactions/Transaction.php
@@ -188,7 +188,7 @@ class Transaction
             $buffer->fill(21);
         }
 
-        if (isset($this->vendorField) && strlen($this->vendorField) < 64) {
+        if (isset($this->vendorField) && strlen($this->vendorField) <= 64) {
             $buffer->writeString($this->vendorField);
             $vendorFieldLength = strlen($this->vendorField);
 
