3d8996dcd4e4767f8096afdce9cbff9ed2d8d627
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-08-01 09:39:09 -0600

    remove disclaimer

--- src/ByteBuffer/Concerns/Reads/UnsignedInteger.php
+++ src/ByteBuffer/Concerns/Reads/UnsignedInteger.php
@@ -2,14 +2,7 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Ark PHP Crypto.
- *
- * (c) Ark Ecosystem <info@ark.io>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 namespace ArkEcosystem\Crypto\ByteBuffer\Concerns\Reads;
 
