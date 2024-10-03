fa98c51afb7dc58bc490ea9026a092a03fbb2a41
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-10-03 17:05:29 -0600

    wip

---
+++ tests/Unit/Transactions/Builder/EvmCallBuilderTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArkEcosystem\Tests\Crypto\Unit\Transactions\Builder;
+
+use ArkEcosystem\Tests\Crypto\TestCase;
+use ArkEcosystem\Crypto\Identities\PublicKey;
+use ArkEcosystem\Crypto\Transactions\Builder\EvmCallBuilder;
+use ArkEcosystem\Crypto\Transactions\Builder\MultiPaymentBuilder;
+
+/**
+ * @covers \ArkEcosystem\Crypto\Transactions\Builder\MultiPaymentBuilder
+ */
+class EvmCallBuilderTest extends TestCase
+{
+    /** @test */
+    public function it_should_sign_it_with_a_passphrase()
+    {
+        $fixture = $this->getTransactionFixture('evm_call', 'evm-sign');
+
+        $builder = EvmCallBuilder::new()
+          ->withFee($fixture['data']['fee'])
+          ->withNonce($fixture['data']['nonce'])
+          ->recipient($fixture['data']['contractId']) 
+          ->payload($fixture['data']['payload'])      
+          ->gasLimit($fixture['data']['gasLimit'])
+          ->sign($this->passphrase);
+
+        $this->assertTrue($builder->verify());
+    }
+}
