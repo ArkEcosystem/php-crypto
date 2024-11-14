0b9b1a4d7662942668e5870c993c00ee4cffef35
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-11-14 10:58:28 -0600

    fix validator registration builder

---
+++ tests/Unit/Transactions/Builder/ValidatorRegistrationBuilderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArkEcosystem\Tests\Crypto\Unit\Transactions\Builder;
+
+use ArkEcosystem\Crypto\Transactions\Builder\ValidatorRegistrationBuilder;
+use ArkEcosystem\Tests\Crypto\TestCase;
+
+/**
+ * @covers \ArkEcosystem\Crypto\Transactions\Builder\ValidatorRegistrationBuilder
+ */
+class ValidatorRegistrationBuilderTest extends TestCase
+{
+    /** @test */
+    public function it_should_sign_it_with_a_passphrase()
+    {
+        $fixture = $this->getTransactionFixture('evm_call', 'validator-registration');
+
+        $builder = ValidatorRegistrationBuilder::new()
+          ->gasPrice($fixture['data']['gasPrice'])
+          ->nonce($fixture['data']['nonce'])
+          ->network($fixture['data']['network'])
+          ->gasLimit($fixture['data']['gasLimit'])
+          ->validatorPublicKey('a08058db53e2665c84a40f5152e76dd2b652125a6079130d4c315e728bcf4dd1dfb44ac26e82302331d61977d3141118')
+          ->recipientAddress($fixture['data']['recipientAddress'])
+          ->sign($this->passphrase);
+
+        $this->assertSame($fixture['serialized'], $builder->transaction->serialize()->getHex());
+        
+        $this->assertSame($fixture['data']['id'], $builder->transaction->data['id']);
+        
+        $this->assertTrue($builder->verify());
+    }
+}
