1d9c758da7bcda1d9bcd9a4a232510573494fcda
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-11-14 10:59:47 -0600

    add rest of builder tests

---
+++ tests/Unit/Transactions/Builder/TransferBuilderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArkEcosystem\Tests\Crypto\Unit\Transactions\Builder;
+
+use ArkEcosystem\Crypto\Transactions\Builder\TransferBuilder;
+use ArkEcosystem\Tests\Crypto\TestCase;
+
+/**
+ * @covers \ArkEcosystem\Crypto\Transactions\Builder\TransferBuilder
+ */
+class TransferBuilderTest extends TestCase
+{
+    /** @test */
+    public function it_should_sign_it_with_a_passphrase()
+    {
+        $fixture = $this->getTransactionFixture('evm_call', 'transfer');
+
+        $builder = TransferBuilder::new()
+          ->gasPrice($fixture['data']['gasPrice'])
+          ->nonce($fixture['data']['nonce'])
+          ->network($fixture['data']['network'])
+          ->gasLimit($fixture['data']['gasLimit'])
+          ->recipientAddress($fixture['data']['recipientAddress'])
+          ->value($fixture['data']['value'])
+          ->sign($this->passphrase);
+
+        $this->assertSame($fixture['serialized'], $builder->transaction->serialize()->getHex());
+        
+        $this->assertSame($fixture['data']['id'], $builder->transaction->data['id']);
+        
+        $this->assertTrue($builder->verify());
+    }
+}
---
+++ tests/Unit/Transactions/Builder/UnvoteBuilderTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArkEcosystem\Tests\Crypto\Unit\Transactions\Builder;
+
+use ArkEcosystem\Crypto\Transactions\Builder\UnvoteBuilder;
+use ArkEcosystem\Tests\Crypto\TestCase;
+
+/**
+ * @covers \ArkEcosystem\Crypto\Transactions\Builder\UnvoteBuilder
+ */
+class UnvoteBuilderTest extends TestCase
+{
+    /** @test */
+    public function it_should_sign_it_with_a_passphrase()
+    {
+        $fixture = $this->getTransactionFixture('evm_call', 'unvote');
+
+        $builder = UnvoteBuilder::new()
+          ->gasPrice($fixture['data']['gasPrice'])
+          ->nonce($fixture['data']['nonce'])
+          ->network($fixture['data']['network'])
+          ->gasLimit($fixture['data']['gasLimit'])
+          ->recipientAddress($fixture['data']['recipientAddress'])
+          ->sign($this->passphrase);
+
+        $this->assertSame($fixture['serialized'], $builder->transaction->serialize()->getHex());
+        
+        $this->assertSame($fixture['data']['id'], $builder->transaction->data['id']);
+        
+        $this->assertTrue($builder->verify());
+    }
+}
