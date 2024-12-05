faf69a23edd2a297e910b77621a50b9d5576c23f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-12-05 17:35:00 +0000

    refactor: argument decoder

---
+++ src/Utils/Abi/ArgumentDecoder.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArkEcosystem\Crypto\Utils\Abi;
+
+use ArkEcosystem\Crypto\Utils\AbiDecoder;
+
+final class ArgumentDecoder
+{
+    private string $bytes;
+
+    public function __construct(string $bytes)
+    {
+        $bytes = hex2bin($bytes);
+        if ($bytes === false) {
+            $bytes = '';
+        }
+
+        $this->bytes = $bytes;
+    }
+
+    public function decodeAddress(): string
+    {
+        [$value] = AbiDecoder::decodeAddress($this->bytes, 0);
+
+        return $value;
+    }
+
+    public function decodeUnsignedInt(): string
+    {
+        [$value] = AbiDecoder::decodeNumber($this->bytes, 0, 32, false);
+
+        return $value;
+    }
+
+    public function decodeSignedInt(): string
+    {
+        [$value] = AbiDecoder::decodeNumber($this->bytes, 0, 32, true);
+
+        return $value;
+    }
+
+    public function decodeBool(): bool
+    {
+        [$value] = AbiDecoder::decodeBool($this->bytes, 0);
+
+        return $value;
+    }
+}
