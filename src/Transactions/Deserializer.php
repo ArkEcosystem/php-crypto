24346ee668bd69602af6d31be1e2044710109699
ItsANameToo <itsanametoo@protonmail.com>
2024-12-17 16:45:15 +0100

    wip

--- src/Transactions/Deserializer.php
+++ src/Transactions/Deserializer.php
@@ -66,7 +66,6 @@ class Deserializer
 
     private function guessTransactionFromData(array $data): AbstractTransaction
     {
-        // TODO: change to check for payload
         if ($data['value'] !== '0') {
             return new Transfer($data);
         }
