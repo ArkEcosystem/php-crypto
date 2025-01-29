41360dc28d6224353192949b25d8c3c40aa5d0a1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-01-29 18:36:38 +0000

    updates

--- tests/Unit/Transactions/DeserializerTest.php
+++ tests/Unit/Transactions/DeserializerTest.php
@@ -37,49 +37,49 @@ class DeserializerTest extends TestCase
         expect($transaction)->toBeInstanceOf(Transfer::class);
     }
 
-    // /** @test */
-    // public function it_should_deserialize_a_vote_signed_with_a_passphrase()
-    // {
-    //     $fixture = $this->getTransactionFixture('evm_call', 'vote');
+    /** @test */
+    public function it_should_deserialize_a_vote_signed_with_a_passphrase()
+    {
+        $fixture = $this->getTransactionFixture('evm_call', 'vote');
 
-    //     $transaction = $this->assertTransaction($fixture);
+        $transaction = $this->assertTransaction($fixture);
 
-    //     expect($transaction->data['vote'])->toEqual('0xC3bBE9B1CeE1ff85Ad72b87414B0E9B7F2366763');
+        expect($transaction->data['vote'])->toEqual('0xC3bBE9B1CeE1ff85Ad72b87414B0E9B7F2366763');
 
-    //     expect($transaction->data['id'])->toEqual('f5d593dd90a22301aa1f8418e6e208d9bd5bbe7806a30a52c3149f4b72338993');
+        expect($transaction->data['id'])->toEqual('991a3a63dc47be84d7982acb4c2aae488191373f31b8097e07d3ad95c0997e69');
 
-    //     expect($transaction)->toBeInstanceOf(Vote::class);
-    // }
+        expect($transaction)->toBeInstanceOf(Vote::class);
+    }
 
-    // /** @test */
-    // public function it_should_deserialize_a_unvote_signed_with_a_passphrase()
-    // {
-    //     $fixture = $this->getTransactionFixture('evm_call', 'unvote');
+    /** @test */
+    public function it_should_deserialize_a_unvote_signed_with_a_passphrase()
+    {
+        $fixture = $this->getTransactionFixture('evm_call', 'unvote');
 
-    //     $transaction = $this->assertTransaction($fixture);
+        $transaction = $this->assertTransaction($fixture);
 
-    //     expect($transaction)->toBeInstanceOf(Unvote::class);
-    // }
+        expect($transaction)->toBeInstanceOf(Unvote::class);
+    }
 
-    // /** @test */
-    // public function it_should_deserialize_a_validator_registration_signed_with_a_passphrase()
-    // {
-    //     $fixture = $this->getTransactionFixture('evm_call', 'validator-registration');
+    /** @test */
+    public function it_should_deserialize_a_validator_registration_signed_with_a_passphrase()
+    {
+        $fixture = $this->getTransactionFixture('evm_call', 'validator-registration');
 
-    //     $transaction = $this->assertTransaction($fixture);
+        $transaction = $this->assertTransaction($fixture);
 
-    //     expect($transaction)->toBeInstanceOf(ValidatorRegistration::class);
-    // }
+        expect($transaction)->toBeInstanceOf(ValidatorRegistration::class);
+    }
 
-    // /** @test */
-    // public function it_should_deserialize_a_validator_resignation_signed_with_a_passphrase()
-    // {
-    //     $fixture = $this->getTransactionFixture('evm_call', 'validator-resignation');
+    /** @test */
+    public function it_should_deserialize_a_validator_resignation_signed_with_a_passphrase()
+    {
+        $fixture = $this->getTransactionFixture('evm_call', 'validator-resignation');
 
-    //     $transaction = $this->assertTransaction($fixture);
+        $transaction = $this->assertTransaction($fixture);
 
-    //     expect($transaction)->toBeInstanceOf(ValidatorResignation::class);
-    // }
+        expect($transaction)->toBeInstanceOf(ValidatorResignation::class);
+    }
 
     private function assertTransaction(array $fixture): AbstractTransaction
     {
@@ -91,7 +91,6 @@ class DeserializerTest extends TestCase
             'gasPrice',
             'gasLimit',
             'contractId',
-            'signature',
             'senderPublicKey',
             'senderAddress',
             'recipientAddress',
