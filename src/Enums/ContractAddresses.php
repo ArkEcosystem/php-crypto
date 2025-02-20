ec65ccbf4a95b0c8de68a37ca9c3c4ca5a73784f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-20 15:09:26 +0000

    update multipayment contract address

--- src/Enums/ContractAddresses.php
+++ src/Enums/ContractAddresses.php
@@ -7,6 +7,6 @@ namespace ArkEcosystem\Crypto\Enums;
 enum ContractAddresses: string
 {
     case CONSENSUS    = '0x535B3D7A252fa034Ed71F0C53ec0C6F784cB64E1';
-    case MULTIPAYMENT = '0x83769BeEB7e5405ef0B7dc3C66C43E3a51A6d27f';
+    case MULTIPAYMENT = '0x00EFd0D4639191C49908A7BddbB9A11A994A8527';
     case USERNAMES    = '0x2c1DE3b4Dbb4aDebEbB5dcECAe825bE2a9fc6eb6';
 }
