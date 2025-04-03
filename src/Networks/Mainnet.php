b40a3414feea72699006e3dbfb99cc1e129fa319
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-04-03 15:27:29 +0100

    refactor: add network wif method

--- src/Networks/Mainnet.php
+++ src/Networks/Mainnet.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace ArkEcosystem\Crypto\Networks;
 
-class Mainnet extends AbstractNetwork
+class Mainnet extends AbstractNetwork implements NetworkInterface
 {
     /**
      * {@inheritdoc}
@@ -15,21 +15,11 @@ class Mainnet extends AbstractNetwork
         self::BASE58_WIF => 'ba', // 186
     ];
 
-    /**
-     * Get the chain identifier.
-     *
-     * @return int
-     */
     public function chainId(): int
     {
         return 10000;
     }
 
-    /**
-     * Get the network epoch.
-     *
-     * @return string
-     */
     public function epoch(): string
     {
         return '2017-03-21T13:00:00.000Z';
