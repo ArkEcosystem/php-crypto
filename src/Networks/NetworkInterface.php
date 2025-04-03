13cf8cf894033a6468fb145645edd6ce9732a3c8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-04-03 15:27:41 +0100

    add interface

---
+++ src/Networks/NetworkInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArkEcosystem\Crypto\Networks;
+
+interface NetworkInterface {
+    /**
+     * Get the chain identifier.
+     *
+     * @return int
+     */
+    public function chainId(): int;
+
+    /**
+     * Get the network epoch.
+     *
+     * @return string
+     */
+    public function epoch(): string;
+
+    /**
+     * Get the network WIF prefix.
+     *
+     * @return string
+     */
+    public function wif(): string;
+}
