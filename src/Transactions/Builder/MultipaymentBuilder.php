170a776631c45cc86c1f74bdeea45ff7796d479a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-04-17 12:46:02 +0100

    rename amount arg

--- src/Transactions/Builder/MultipaymentBuilder.php
+++ src/Transactions/Builder/MultipaymentBuilder.php
@@ -21,14 +21,14 @@ class MultipaymentBuilder extends AbstractTransactionBuilder
         $this->transaction->refreshPayloadData();
     }
 
-    public function pay(string $address, BigDecimal $amount): self
+    public function pay(string $address, BigDecimal $value): self
     {
         $this->transaction->data['pay'][0][] = $address;
-        $this->transaction->data['pay'][1][] = $amount;
+        $this->transaction->data['pay'][1][] = $value;
 
         $this->transaction->refreshPayloadData();
 
-        $this->transaction->data['value'] = $this->transaction->data['value']->plus($amount);
+        $this->transaction->data['value'] = $this->transaction->data['value']->plus($value);
 
         return $this;
     }
