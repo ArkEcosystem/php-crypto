c5b717b1efcb93c7e893512f1f33e24cdc8ee21b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-06-11 01:03:13 +0100

    test: binary integer reader writer coverage

---
+++ tests/Unit/Binary/Integer/ReaderTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use ArkEcosystem\Crypto\Binary\Integer\Reader;
+
+it('should read a signed 8 bit negative integer', function () {
+    $data = pack('c', -128);
+    $result = Reader::bit8($data);
+    expect($result)->toBe(-128);
+});
+
+it('should read a signed 8 bit positive integer', function () {
+    $data = pack('c', 127);
+    $result = Reader::bit8($data);
+    expect($result)->toBe(127);
+});
+
+it('should read a signed 16 bit negative integer', function () {
+    $data = pack('s', -32768);
+    $result = Reader::bit16($data);
+    expect($result)->toBe(-32768);
+});
+
+it('should read a signed 16 bit positive integer', function () {
+    $data = pack('s', 32767);
+    $result = Reader::bit16($data);
+    expect($result)->toBe(32767);
+});
+
+it('should read a signed 32 bit negative integer', function () {
+    $data = pack('l', -2147483648);
+    $result = Reader::bit32($data);
+    expect($result)->toBe(-2147483648);
+});
+
+it('should read a signed 32 bit positive integer', function () {
+    $data = pack('l', 2147483647);
+    $result = Reader::bit32($data);
+    expect($result)->toBe(2147483647);
+});
+
+it('should read a signed 64 bit negative integer', function () {
+    $data = pack('q', -9223372036854775807);
+    $result = Reader::bit64($data);
+    expect($result)->toBe(-9223372036854775807);
+});
+
+it('should read a signed 64 bit positive integer', function () {
+    $data = pack('q', 9223372036854775807);
+    $result = Reader::bit64($data);
+    expect($result)->toBe(9223372036854775807);
+});
---
+++ tests/Unit/Binary/Integer/WriterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use ArkEcosystem\Crypto\Binary\Integer\Writer;
+
+it('should write a signed 8 bit negative integer', function () {
+    $data = -128;
+    $result = Writer::bit8($data);
+    expect($result)->toBe(pack('c', $data));
+});
+
+it('should write a signed 8 bit positive integer', function () {
+    $data = 127;
+    $result = Writer::bit8($data);
+    expect($result)->toBe(pack('c', $data));
+});
+
+it('should write a signed 16 bit negative integer', function () {
+    $data = -32768;
+    $result = Writer::bit16($data);
+    expect($result)->toBe(pack('s', $data));
+});
+
+it('should write a signed 16 bit positive integer', function () {
+    $data = 32767;
+    $result = Writer::bit16($data);
+    expect($result)->toBe(pack('s', $data));
+});
+
+it('should write a signed 32 bit negative integer', function () {
+    $data = -2147483648;
+    $result = Writer::bit32($data);
+    expect($result)->toBe(pack('l', $data));
+});
+
+it('should write a signed 32 bit positive integer', function () {
+    $data = 2147483647;
+    $result = Writer::bit32($data);
+    expect($result)->toBe(pack('l', $data));
+});
+
+it('should write a signed 64 bit negative integer', function () {
+    $data = -9223372036854775807;
+    $result = Writer::bit64($data);
+    expect($result)->toBe(pack('q', $data));
+});
+
+it('should write a signed 64 bit positive integer', function () {
+    $data = 9223372036854775807;
+    $result = Writer::bit64($data);
+    expect($result)->toBe(pack('q', $data));
+});
