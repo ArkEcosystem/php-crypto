588afe61f9086a4dd612bc44cd9b300a3aa90605
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-06-16 14:23:13 +0100

    abi function enum tests

---
+++ tests/Unit/Enums/AbiFunctionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use ArkEcosystem\Crypto\Enums\AbiFunction;
+use ArkEcosystem\Crypto\Transactions\Types\Multipayment;
+use ArkEcosystem\Crypto\Transactions\Types\Unvote;
+use ArkEcosystem\Crypto\Transactions\Types\UsernameRegistration;
+use ArkEcosystem\Crypto\Transactions\Types\UsernameResignation;
+use ArkEcosystem\Crypto\Transactions\Types\ValidatorRegistration;
+use ArkEcosystem\Crypto\Transactions\Types\ValidatorResignation;
+use ArkEcosystem\Crypto\Transactions\Types\Vote;
+
+it('should get transaction class', function ($type, $class) {
+    expect(AbiFunction::{$type}->transactionClass())->toEqual($class);
+})->with([
+    'Vote' => ['VOTE', Vote::class],
+    'Unvote' => ['UNVOTE', Unvote::class],
+    'ValidatorRegistration' => ['VALIDATOR_REGISTRATION', ValidatorRegistration::class],
+    'ValidatorResignation' => ['VALIDATOR_RESIGNATION', ValidatorResignation::class],
+    'UsernameRegistration' => ['USERNAME_REGISTRATION', UsernameRegistration::class],
+    'UsernameResignation' => ['USERNAME_RESIGNATION', UsernameResignation::class],
+    'Multipayment' => ['MULTIPAYMENT', Multipayment::class],
+]);
