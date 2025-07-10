b4b71f582c8d6202f1f51d9a889489a74581fccf
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-07-10 16:21:08 +0100

    chore: update network chain id (#180)

--- tests/Unit/Networks/MainnetTest.php
+++ tests/Unit/Networks/MainnetTest.php
@@ -10,13 +10,9 @@ use ArkEcosystem\Crypto\Networks\Mainnet;
  * @covers \ArkEcosystem\Crypto\Networks\Mainnet
  */
 
-$epoch = '2017-03-21T13:00:00.000Z';
-$wif   = 'ba';
-
-/**
- * @todo: adjust the $chainId once known
- */
-$chainId = 10000;
+$epoch   = '2017-03-21T13:00:00.000Z';
+$wif     = 'ba';
+$chainId = 11811;
 
 beforeEach(function () use ($epoch, $chainId, $wif) {
     $this->chainId = $chainId;
