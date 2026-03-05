564ca1a7170b5329e043e55584d874a2f4db59a1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-03-05 11:50:37 +0000

    feat: add batch transfer abi

--- src/Enums/ContractAbiType.php
+++ src/Enums/ContractAbiType.php
@@ -6,8 +6,9 @@ namespace ArkEcosystem\Crypto\Enums;
 
 enum ContractAbiType: string
 {
-    case CUSTOM       = 'custom';
-    case CONSENSUS    = 'consensus';
-    case MULTIPAYMENT = 'multipayment';
-    case USERNAMES    = 'usernames';
+    case CUSTOM              = 'custom';
+    case CONSENSUS           = 'consensus';
+    case MULTIPAYMENT        = 'multipayment';
+    case USERNAMES           = 'usernames';
+    case ERC20BATCH_TRANSFER = 'erc20batchtransfer';
 }
--- src/Utils/AbiBase.php
+++ src/Utils/AbiBase.php
@@ -84,6 +84,8 @@ abstract class AbiBase
                 return __DIR__.'/Abi/json/Abi.Multipayment.json';
             case ContractAbiType::USERNAMES:
                 return __DIR__.'/Abi/json/Abi.Usernames.json';
+            case ContractAbiType::ERC20BATCH_TRANSFER:
+                return __DIR__.'/Abi/json/Abi.ERC20BatchTransfer.json';
             case ContractAbiType::CUSTOM:
                 return $path;
         }
