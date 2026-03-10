d88de2aaa3003256254f258510734b66b5b915b7
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-03-10 14:11:02 +0000

    refactor: remaining coverage (#178)

--- tests/Unit/Utils/TransactionUtilsTest.php
+++ tests/Unit/Utils/TransactionUtilsTest.php
@@ -35,7 +35,8 @@ it('should get the hash for a transaction', function () {
 it('should handle string data starting with 0x', function () {
     $fixture = $this->getTransactionFixture('evm_call', 'username-resignation');
 
-    $fixture['data']['nonce'] = '0x'.dechex((int) $fixture['data']['nonce']);
+    $fixture['data']['gasPrice'] = '0x'.dechex((int) $fixture['data']['gasPrice']);
+    $fixture['data']['nonce']    = '0x'.dechex((int) $fixture['data']['nonce']);
 
     $transaction = TransactionUtils::toBuffer($fixture['data']);
 
