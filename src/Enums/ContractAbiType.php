45d538253819cde30e19408475786b86f1b98173
ItsANameToo <itsanametoo@protonmail.com>
2026-03-10 15:26:28 +0100

    wip

--- src/Enums/ContractAbiType.php
+++ src/Enums/ContractAbiType.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace ArkEcosystem\Crypto\Enums;
 
 enum ContractAbiType: string
-
+{
     case CUSTOM              = 'custom';
     case CONSENSUS           = 'consensus';
     case ERC20BATCH_TRANSFER = 'erc20batchtransfer';
